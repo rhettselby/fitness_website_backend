680a8c78641bcbd2c082172b746f1d60cff969bb
Rhett Selby <rhettselby10@gmail.com>
2025-12-05 22:57:24 -0800

    ooo

--- myBackend/settings.py
+++ myBackend/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
     "default": dj_database_url.parse(
         os.environ.get("DATABASE_URL"),
         conn_max_age=600,
-        ssl_require=IS_RAILWAY,
+        ssl_require=False,
     )
 }
 
