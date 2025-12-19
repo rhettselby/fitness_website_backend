247dbacd1fbf5a063792199699ecabf47b4034bb
Rhett Selby <rhettselby10@gmail.com>
2025-12-18 18:05:13 -0600

    debug

--- myBackend/urls.py
+++ myBackend/urls.py
@@ -19,6 +19,10 @@ from django.urls import path, include
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
    #path('admin/', admin.site.urls),
@@ -49,6 +53,11 @@ urlpatterns = [
     path('users/', include('users.urls')),  # This already has /api/ paths inside
     path('api/fitness/', include('fitness.urls')),  # This already has /api/ paths inside
     path('api/profile/', include('profile_page.urls')),
+
+    #JWT Authentication Routes
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
 
 ]
 
--- users/urls.py
+++ users/urls.py
@@ -17,4 +17,10 @@ urlpatterns = [
     path('api/logout/', views.logout_view_api, name = "logout_api"),
     path('api/check-auth/', views.check_auth_api, name = "check_auth_api"),
     path("api/csrf/", views.csrf),
+
+    ### JWT Authentication Urls
+    path('api/register-jwt/', views.register_api_jwt, name='register_jwt'),
+    path('api/login-jwt/', views.login_view_api_jwt, name='login_jwt'),
+    path('api/check-auth-jwt/', views.check_auth_api_jwt, name='check_auth_jwt'),
+
 ]
