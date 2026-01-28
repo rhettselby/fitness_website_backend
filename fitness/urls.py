542927085f0eaf5d1ee9d377bbb2ad3051a8a9ca
Rhett Selby <rhettselby10@gmail.com>
2026-01-27 21:59:11 -0800

    debug

--- fitness/urls.py
+++ fitness/urls.py
@@ -20,6 +20,6 @@ urlpatterns = [
   path('api/add/gym-jwt/', views.add_gym_api_jwt, name = "add_gym_jwt"),
   path('api/add/cardio-jwt/', views.add_cardio_api_jwt, name = "add_cardio_jwt"),
 
-  path("api/comments/<str:workout_type/<int:workout_id>/", views.get_comments_api, name='get_comments_api'),
+  path("api/comments/<str:workout_type>/<int:workout_id>/", views.get_comments_api, name='get_comments_api'),
   path("api/comments/", views.add_comment_api_jwt, name = 'add_comment_api_jwt'),
 ]
