abf08233538e6f9c614f8b6321a429917c606225
Rhett Selby <rhettselby10@gmail.com>
2026-04-18 16:21:52 -0700

    updated add activity urls

--- fitness/urls.py
+++ fitness/urls.py
@@ -5,10 +5,10 @@ app_name = 'fitness'
 
 urlpatterns = [
      #jwt authentication url's
-
-  path('api/add/gym-jwt/', views.add_gym_api_jwt, name = "add_gym_jwt"),
-  path('api/add/cardio-jwt/', views.add_cardio_api_jwt, name = "add_cardio_jwt"),
-
+  path('add/gym/', views.add_gym_api_jwt, name="add_gym_jwt"),
+  path('add/cardio/', views.add_cardio_api_jwt, name="add_cardio_jwt"),
+  path('add/sport/', views.add_sport, name="add_sport"),
+  path('add/booze/', views.add_booze, name="add_booze"),
   path("api/comments/<str:workout_type>/<int:workout_id>/", views.get_comments_api, name='get_comments_api'),
   path("api/comments/", views.add_comment_api_jwt, name = 'add_comment_api_jwt'),
 ]
